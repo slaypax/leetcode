f173c635b7b9bf55fafc0bf7c7164308b4ab4b60
keviryan <keviryan@amazon.com>
2019-04-17 21:57:30 -0500

    tinyUrl problem

---
+++ encode_and_decode_tinyurl.py
@@ -0,0 +1,33 @@
+# https://leetcode.com/problems/encode-and-decode-tinyurl
+# TinyURL is a URL shortening service where you enter a URL such as 
+# https://leetcode.com/problems/design-tinyurl and it returns a short URL such
+# as http://tinyurl.com/4e9iAk.
+
+
+# Runtime: 24 ms, faster than 93.85% of Python online submissions for Encode and Decode TinyURL.
+import hashlib
+
+class Codec:
+    def __init__(self):
+        self.url_map = {}
+        self.prefix = 'https://tinyurl.com/'
+        
+    def encode(self, longUrl):
+        urlHash = hashlib.md5(b'longUrl').hexdigest()[0:6]
+        newUrl = "{prefix}{urlhash}".format(prefix=self.prefix, urlhash=urlHash)
+        self.url_map[newUrl] = longUrl
+        return newUrl
+        
+
+    def decode(self, shortUrl):
+        """Decodes a shortened URL to its original URL.
+        
+        :type shortUrl: str
+        :rtype: str
+        """
+        return self.url_map.get(shortUrl)
+        
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(url))
