7ceebb091d07473bf8f3e6e214a222498c00eedc
keviryan <keviryan@amazon.com>
2019-04-17 22:05:48 -0500

    more readable tinyurl example

--- encode_and_decode_tinyurl.py
+++ encode_and_decode_tinyurl.py
@@ -14,9 +14,9 @@ class Codec:
         
     def encode(self, longUrl):
         urlHash = hashlib.md5(b'longUrl').hexdigest()[0:6]
-        newUrl = "{prefix}{urlhash}".format(prefix=self.prefix, urlhash=urlHash)
-        self.url_map[newUrl] = longUrl
-        return newUrl
+        shortUrl = "{prefix}{urlhash}".format(prefix=self.prefix, urlhash=urlHash)
+        self.url_map[shortUrl] = longUrl
+        return shortUrl
         
 
     def decode(self, shortUrl):
