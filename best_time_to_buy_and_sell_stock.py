e71f3aa6fd8b28e81d644b81261e53f98f7950f1
slaypax <k.ryan120c@gmail.com>
2019-05-17 00:10:12 -0500

    buy low sell high

---
+++ best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,25 @@
+# brute force, times out on big list.
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0
+        for i in range(len(prices) - 1):
+            for j in range(i + 1, len(prices)):
+                profit = prices[j] - prices[i]
+                if profit > max_profit:
+                    max_profit = profit
+        return max_profit
+
+
+import sys
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price = sys.maxsize
+        max_profit = 0
+        for i in range(len(prices)):
+            if prices[i] < min_price:
+                min_price = prices[i]
+            elif prices[i] - min_price > max_profit:
+                max_profit = prices[i] - min_price
+        return max_profit
